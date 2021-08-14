adb2b9a601810a9d1a7c7fb8be4cf5b57355aa1b
id2k1149 <id2k1149@gmail.com>
2021-08-14 13:42:11 -0400

    v09 token refresh

--- src/main/java/org/id2k1149/project_v10/filter/AuthorizationFilter.java
+++ src/main/java/org/id2k1149/project_v10/filter/AuthorizationFilter.java
@@ -34,7 +34,7 @@ public class AuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().equals("/api/login")){
+        if (request.getServletPath().equals("/api/login") || request.getServletPath().equals("/api/token/refresh")){
             filterChain.doFilter(request, response);
         } else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
