4d9f79f073fba1e3e47f602d963d8460f25754f0
id2k1149 <id2k1149@gmail.com>
2021-08-14 17:17:51 -0400

    v10 saveRoleTest

--- src/main/java/org/id2k1149/project_v10/security/SecurityConfig.java
+++ src/main/java/org/id2k1149/project_v10/security/SecurityConfig.java
@@ -1,8 +1,7 @@
 package org.id2k1149.project_v10.security;
 
 import lombok.RequiredArgsConstructor;
-import org.id2k1149.project_v10.filter.AuthenticationFilter;
-import org.id2k1149.project_v10.filter.AuthorizationFilter;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -32,17 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManagerBean());
-        authenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh").permitAll();
+        http.authorizeRequests().antMatchers("/api/login/**").permitAll();
         http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
-        http.addFilter(authenticationFilter);
-        http.addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+
+          }
 
     @Bean
     @Override
---
+++ src/test/java/org/id2k1149/project_v10/repo/RoleRepoTest.java
@@ -0,0 +1,29 @@
+package org.id2k1149.project_v10.repo;
+
+import com.github.javafaker.Faker;
+import org.id2k1149.project_v10.model.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+@Rollback(false)
+public class RoleRepoTest {
+    private final Faker faker = new Faker();
+
+    @Autowired
+    private RoleRepo roleRepo;
+
+    @Test
+    public void test() {
+        roleRepo.save(new Role(null,"ROLE_ADMIN"));
+        roleRepo.save(new Role(null,"ROLE_USER"));
+    }
+
+
+}
