fbff5aa2600a2ac50002ac1607603c1f74ff4c6d
id2k1149 <id2k1149@gmail.com>
2021-08-14 19:37:23 -0400

    v12 UserController

--- src/main/java/org/id2k1149/project_v10/ProjectV10Application.java
+++ src/main/java/org/id2k1149/project_v10/ProjectV10Application.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ProjectV10Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectV10Application.class, args);
 	}
@@ -17,40 +16,4 @@ public class ProjectV10Application {
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
-	/*
-	@Bean
-	CommandLineRunner run(UserService userService) {
-		return args -> {
-			userService.saveRole(new Role1(null,"ROLE_USER"));
-			userService.saveRole(new Role1(null,"ROLE_ADMIN"));
-
-			userService.saveUser(new User(
-					null,
-					"Brian",
-					"admin1",
-					"password",
-					new ArrayList<>()));
-			userService.saveUser(new User(
-					null,
-					"John",
-					"user_1",
-					"password",
-					new ArrayList<>()));
-			userService.saveUser(new User(
-					null,
-					"Paul",
-					"user_2",
-					"password",
-					new ArrayList<>()));
-
-			userService.addRoleToUser("admin1", "ROLE_ADMIN");
-			userService.addRoleToUser("admin1", "ROLE_USER");
-			userService.addRoleToUser("user_1", "ROLE_USER");
-			userService.addRoleToUser("user_1", "ROLE_ADMIN");
-			userService.addRoleToUser("user_2", "ROLE_USER");
-		};
-	}
-
-	 */
 }
