528f6bc474773035b5cc0a2ec57d87a4feeaae4b
id2k1149 <id2k1149@gmail.com>
2021-08-14 21:17:52 -0400

    v16 SecurityService

---
+++ src/main/java/org/id2k1149/project_v10/webController/UserWebController.java
@@ -0,0 +1,65 @@
+package org.id2k1149.project_v10.webController;
+
+import org.id2k1149.project_v10.model.User;
+import org.id2k1149.project_v10.security.SecurityService;
+import org.id2k1149.project_v10.service.UserService;
+import org.id2k1149.project_v10.validator.UserValidator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserWebController {
+    private final UserService userService;
+    private final SecurityService securityService;
+    private final UserValidator userValidator;
+
+    public UserWebController(UserService userService,
+                             SecurityService securityService,
+                             UserValidator userValidator) {
+        this.userService = userService;
+        this.securityService = securityService;
+        this.userValidator = userValidator;
+    }
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "auth/registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute("userForm") User userForm,
+                               BindingResult bindingResult) {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "auth/registration";
+        }
+
+        userService.addUser(userForm);
+        securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
+
+        return "redirect:/vote";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username or password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "auth/login";
+    }
+
+    @GetMapping({"/", "/index", "/welcome"})
+    public String welcome(Model model) {
+        return "welcome";
+    }
+}
---
+++ src/main/java/org/id2k1149/project_v10/webController/VoteWebController.java
@@ -0,0 +1,63 @@
+package org.id2k1149.project_v10.webController;
+
+import org.id2k1149.project_v10.model.Counter;
+import org.id2k1149.project_v10.model.Voter;
+import org.id2k1149.project_v10.service.CounterService;
+import org.id2k1149.project_v10.service.InfoService;
+import org.id2k1149.project_v10.service.VoterService;
+import org.id2k1149.project_v10.to.AnswerTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Controller
+public class VoteWebController {
+    private final InfoService infoService;
+    private final CounterService counterService;
+    private final VoterService voterService;
+
+    public VoteWebController(InfoService infoService,
+                             CounterService counterService,
+                             VoterService voterService) {
+
+        this.infoService = infoService;
+        this.counterService = counterService;
+        this.voterService = voterService;
+    }
+
+
+    @GetMapping("/vote")
+    public String survey(Model model) {
+        List<AnswerTo> answersList = infoService.checkTime();
+        if (answersList.size() > 0) {
+            model.addAttribute("answersList", answersList);
+            Optional<Voter> optionalVoter = voterService.checkUser();
+            if (optionalVoter.isPresent()) model.addAttribute("error1", "Your voted today.");
+        } else {
+            model.addAttribute("error2", "It is too late to vote.");
+        }
+        return "vote";
+    }
+
+    @PostMapping("/vote")
+    public String vote(Counter counter) {
+        if (counter.getAnswer() == null) {
+            return "redirect:/vote";
+        }
+        counterService.vote(counter);
+        return "redirect:/result";
+    }
+
+    @GetMapping("/result")
+    public String result(Model model) {
+        List<Counter> sortedList = counterService.getResult();
+        if (sortedList.size() > 0) model.addAttribute("sortedList", sortedList);
+        else model.addAttribute("error", "There are no results");
+        return "result";
+    }
+}
