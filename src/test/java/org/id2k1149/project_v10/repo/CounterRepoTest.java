b46ce389344748519d9621afe15c26d31cfc7256
id2k1149 <id2k1149@gmail.com>
2021-08-16 22:07:04 -0400

    v30 repo tests

--- src/test/java/org/id2k1149/project_v10/repo/CounterRepoTest.java
+++ src/test/java/org/id2k1149/project_v10/repo/CounterRepoTest.java
@@ -4,12 +4,14 @@ import com.github.javafaker.Faker;
 import org.id2k1149.project_v10.model.Answer;
 import org.id2k1149.project_v10.model.Counter;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -24,6 +26,30 @@ class CounterRepoTest {
     @Autowired
     private AnswerRepo testAnswerRepo;
 
+    public Answer getRandomAnswer() {
+        Answer answer = new Answer();
+        answer.setTitle(faker.beer().name());
+        testAnswerRepo.save(answer);
+        return answer;
+    }
+
+    public LocalDate getRandomDate() {
+        long minDay = LocalDate.of(2021, 8, 1).toEpochDay();
+        long maxDay = LocalDate.of(2021, 8, 31).toEpochDay();
+        long randomDay = ThreadLocalRandom.current().nextLong(minDay, maxDay);
+        return LocalDate.ofEpochDay(randomDay);
+    }
+
+    @BeforeEach
+    void setUp() {
+        IntStream.range(0, new Random().nextInt(4) + 2)
+                .mapToObj(i -> new Counter()).forEach(counter -> {
+            counter.setAnswer(getRandomAnswer());
+            counter.setDate(getRandomDate());
+            testCounterRepo.save(counter);
+        });
+    }
+
     @AfterEach
     void tearDown() {
         testAnswerRepo.deleteAll();
@@ -32,39 +58,32 @@ class CounterRepoTest {
 
     @Test
     void findByDateAndAnswer() {
-        Answer answer1 = new Answer();
-        String testTitle = faker.beer().name();
-        answer1.setTitle(testTitle);
-        testAnswerRepo.save(answer1);
+        Answer answer = getRandomAnswer();
+        LocalDate date = getRandomDate();
+        Counter counter = new Counter();
+        counter.setAnswer(answer);
+        counter.setDate(date);
+        testCounterRepo.save(counter);
 
-        Counter counter1 = new Counter();
-        counter1.setAnswer(answer1);
-        counter1.setDate(LocalDate.now());
-        testCounterRepo.save(counter1);
+        Optional<Counter> counter2 = testCounterRepo.findByDateAndAnswer(date, answer);
 
-        Optional<Counter> counter2 = testCounterRepo.findByDateAndAnswer(LocalDate.now(), answer1);
-
-        assertThat(counter2.get()).isEqualTo(counter1);
+        assertThat(counter2.get()).isEqualTo(counter);
     }
 
     @Test
     void findAllByDate() {
-        Random random = new Random();
-        int bound = random.nextInt(4) + 2;
-        LocalDate ld = new java.sql.Date(new Date().getTime()).toLocalDate();
-        List<Counter> list1 = new ArrayList<>();
-        IntStream.range(0, bound).mapToObj(i -> new Counter()).forEach(counter -> {
-            Answer answer = new Answer();
-            answer.setTitle(faker.beer().name());
-            testAnswerRepo.save(answer);
-            counter.setAnswer(answer);
-            counter.setDate(new java.sql.Date(new Date().getTime()).toLocalDate());
-            list1.add(counter);
+        LocalDate date = getRandomDate();
+        List<Counter> list = new ArrayList<>();
+        IntStream.range(0, new Random().nextInt(4) + 2)
+                .mapToObj(i -> new Counter()).forEach(counter -> {
+            counter.setAnswer(getRandomAnswer());
+            counter.setDate(date);
+            list.add(counter);
             testCounterRepo.save(counter);
         });
 
-        List<Counter> list2 = testCounterRepo.findAllByDate(ld);
+        List<Counter> list2 = testCounterRepo.findAllByDate(date);
 
-        assertThat(list2).isEqualTo(list1);
+        assertThat(list2).isEqualTo(list);
     }
 }
--- src/test/java/org/id2k1149/project_v10/repo/UserRepoTest.java
+++ src/test/java/org/id2k1149/project_v10/repo/UserRepoTest.java
@@ -1,11 +1,16 @@
 package org.id2k1149.project_v10.repo;
 
 import com.github.javafaker.Faker;
+import org.id2k1149.project_v10.model.Answer;
 import org.id2k1149.project_v10.model.User;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Random;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -16,6 +21,19 @@ class UserRepoTest {
     @Autowired
     private UserRepo testUserRepo;
 
+    public User getRandomUser() {
+        User user = new User();
+        user.setUsername(faker.name().username());
+        return user;
+    }
+
+    @BeforeEach
+    void setUp() {
+        IntStream.range(0, new Random().nextInt(4) + 2)
+                .mapToObj(i -> getRandomUser())
+                .forEach(user -> testUserRepo.save(user));
+    }
+
     @AfterEach
     void tearDown() {
         testUserRepo.deleteAll();
@@ -23,13 +41,12 @@ class UserRepoTest {
 
     @Test
     void findByUsername() {
-        User user1 = new User();
-        String testName = faker.name().username();
-        user1.setUsername(testName);
-        testUserRepo.save(user1);
+        User user = getRandomUser();
+        String testName = user.getUsername();
+        testUserRepo.save(user);
 
         User user2 = testUserRepo.findByUsername(testName);
 
-        assertThat(user2).isEqualTo(user1);
+        assertThat(user2).isEqualTo(user);
     }
 }
