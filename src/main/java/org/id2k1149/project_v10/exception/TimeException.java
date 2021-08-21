15097ee6c83cf3607e20cbad33dda6db1505c394
alfie <id2k1149@gmail.com>
2021-08-21 13:19:45 -0400

    v57 refactoring

--- src/main/java/org/id2k1149/project_v10/exception/TimeException.java
+++ src/main/java/org/id2k1149/project_v10/exception/TimeException.java
@@ -3,7 +3,7 @@ package org.id2k1149.project_v10.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY, reason = "Time limit")
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY, reason = "Time is over for voting")
 public class TimeException extends RuntimeException {
     public TimeException(String msg) {
         super(msg);
